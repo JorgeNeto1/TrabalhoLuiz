6ca8953da5ed87ee42e3253f7e61db0c8986ef07
Victorminamihara1 <140768019+Victorminamihara1@users.noreply.github.com>
2024-11-27 19:27:38 -0300

    Implementação de função para mostrar os cenário(carosel)

--- Game-TheLegendOfZelda/scriptzelda.js
+++ Game-TheLegendOfZelda/scriptzelda.js
@@ -10,3 +10,22 @@ function abrirGameplay()
 {
     window.location.href = "gameplay.html"; 
 }
+
+function abrirPersonagens() 
+{
+    window.location.href = "personagens.html"; 
+}
+function abrirCuriosidade() 
+{
+    window.location.href = "curiosidades.html"; 
+}
+
+function abrirCenario() 
+{
+    window.location.href = "cenario.html"; 
+}
+
+function voltar() 
+{
+    window.location.href = "../index.html"; 
+}
